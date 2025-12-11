623d4245dac5713e5a4cddff2b774f31e6ac40fd
Tim Supinie <tsupinie@gmail.com>
2025-12-10 22:17:38 -0600

    The surface values and scale factors should be signed ints

--- src/ts/grib2base.ts
+++ src/ts/grib2base.ts
@@ -1,31 +1,40 @@
 
 type G2UInt1Type = 'ui1';
+type G2Int1Type = 'i1';
 type G2UInt2Type = 'ui2';
 type G2Int2Type = 'i2';
 type G2UInt4Type = 'ui4';
 type G2Int4Type = 'i4';
 type G2UInt8Type = 'ui8';
 const G2UInt1 = 'ui1' as G2UInt1Type;
+const G2Int1 = 'i1' as G2Int1Type;
 const G2UInt2 = 'ui2' as G2UInt2Type;
 const G2Int2 = 'i2' as G2Int2Type;
 const G2UInt4 = 'ui4' as G2UInt4Type;
 const G2Int4 = 'i4' as G2Int4Type;
 const G2UInt8 = 'ui8' as G2UInt8Type;
 
-type Grib2InternalType = G2UInt1Type | G2UInt2Type | G2Int2Type | G2UInt4Type | G2Int4Type | G2UInt8Type;
+type Grib2InternalType = G2UInt1Type | G2Int1Type | G2UInt2Type | G2Int2Type | G2UInt4Type | G2Int4Type | G2UInt8Type;
 type Grib2ContentSpecTemplate<T> = Record<string, Grib2InternalType | Grib2TemplateEnumeration<T>>;
 type Grib2Content<T, U> = Record<keyof T, number | U>
 
 function isInternalType<T>(obj: Grib2InternalType | Grib2TemplateEnumeration<T>) : obj is Grib2InternalType {
-    return obj === 'ui1' || obj === 'ui2' || obj == 'i2' || obj === 'ui4' || obj === 'i4' || obj === 'ui8';
+    return obj === 'ui1' || obj === 'i1' || obj === 'ui2' || obj == 'i2' || obj === 'ui4' || obj === 'i4' || obj === 'ui8';
 }
 
 function unpackStruct<T, U>(buf: DataView, struct: Grib2ContentSpecTemplate<U>, offset: number): Grib2Content<T, U> {
     const data_types = {
-        'ui1': () => {  
+        'ui1': () => {
             const val = buf.getUint8(offset);
             offset += 1;
             return val;
+        },
+        'i1': () => {
+            const sign_bit_mask = 1 << 7;
+
+            const val = buf.getUint8(offset);
+            offset += 1;
+            return (val & sign_bit_mask) ? -(val & ~sign_bit_mask) : val; // Grib stores negatives with just a sign bit ...
         },
         'ui2': () => {
             const val = buf.getUint16(offset);
@@ -37,7 +46,7 @@ function unpackStruct<T, U>(buf: DataView, struct: Grib2ContentSpecTemplate<U>, 
 
             const val = buf.getUint16(offset);
             offset += 2;
-            return (val & sign_bit_mask) ? -(val & ~sign_bit_mask) : val; // Grib stores negatives with just a sign bit ...;
+            return (val & sign_bit_mask) ? -(val & ~sign_bit_mask) : val; // Grib stores negatives with just a sign bit ...
         },
         'ui4': () => { 
             const val = buf.getUint32(offset);
@@ -147,5 +156,5 @@ type InternalTypeMapper<T, U=never, V=never> = {
     [K in keyof T]: K extends U ? V : number;
 }
 
-export {Grib2Struct, Grib2TemplateEnumeration, unpackStruct, unpackUTF8String, unpackerFactory, G2UInt1, G2UInt2, G2Int2, G2UInt4, G2Int4, G2UInt8};
+export {Grib2Struct, Grib2TemplateEnumeration, unpackStruct, unpackUTF8String, unpackerFactory, G2UInt1, G2Int1, G2UInt2, G2Int2, G2UInt4, G2Int4, G2UInt8};
 export type {Grib2InternalType,  Grib2Content, Grib2ContentSpecTemplate, Unpackable, InternalTypeMapper, Constructor};
